08a58edce23df29c99bec37be93c45a341d57aae
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-13 11:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name , age){
+		this.name = "Rahul";
+		this.age= age;
+	}
+	get this.name;
+	set this.age = 23
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
